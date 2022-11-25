02c4490e9f84c00ac84f7f854f328b7ac1d3eb45
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-11-25 23:34:46 +0000

    fix prettify logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LoggerModule } from 'nestjs-pino';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LoggerModule } from "nestjs-pino";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 
 const prettifyLogs = LoggerModule.forRoot({
   pinoHttp: {
     transport: {
-      target: 'pino-pretty',
+      target: "pino-pretty",
       options: {
-        singleLine: true,
-      },
-    },
-  },
+        singleLine: true
+      }
+    }
+  }
 });
 
 
 @Module({
-  imports: [
-    !isProd ? prettifyLogs : null
-  ],
+  imports: !isProd ? [prettifyLogs] : [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
