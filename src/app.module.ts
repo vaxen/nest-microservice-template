588ad387078b32f7c0465197ee3750af8e12a7a2
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-11-25 23:03:07 +0000

    added logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+
+const prettifyLogs = LoggerModule.forRoot({
+  pinoHttp: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        singleLine: true,
+      },
+    },
+  },
+});
+
 
 @Module({
-  imports: [],
+  imports: [
+    !isProd ? prettifyLogs : null
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+const port = 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  console.info(`App is running at port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
