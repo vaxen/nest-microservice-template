8123025bfac5229e32c5ea7be12c0b50e9cdf839
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-11-25 23:19:22 +0000

    add heroku settings

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
   console.info(`App is running at port ${port}`);
-  await app.listen(port);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
