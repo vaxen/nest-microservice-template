c9931f48a3e6c4bb9978520798711b6ae553b1d5
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-11-26 01:06:30 +0000

    change endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(1).toBe(1);
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 
+export type HealthCheck = {healthCheck: string;msg: string}
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
 
-  getHello(): string {
+  getHello(): HealthCheck {
     this.logger.log("Inside getHello controller");
-    return 'Hello World!';
+    return {healthCheck: "OK", msg: "Hello From nest microservice demo"};
   }
 }
