328ed45a11b9cd9206f4b6d0aa57036737396e5a
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-11-26 01:14:44 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const port = 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+  app.enableCors();
   console.info(`App is running at port ${port}`);
 
   await app.listen(process.env.PORT || port);
