41ec1db22a873da0f0fad5d5466f8ee315f66480
mattia.pietropaolo <mpp.pietropaolo@gmail.com>
2022-12-07 00:13:33 +0000

    added config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-const port = 8080;
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-
   app.enableCors();
-  console.info(`App is running at port ${port}`);
-
-  await app.listen(process.env.PORT || port);
+  console.info(`App is running at port ${process.env.PORT || config.get("port")}`);
+  console.info(`NODE_ENV is ${process.env.NODE_ENV}`);
+  console.info(`Config loaded is ${config.get("env")}`);
+  await app.listen(process.env.PORT || config.get("port"));
 }
 bootstrap();
